81516fbc93e4f22daf2b426ee352694547dcc2d3
Sinnlos <domin.mal483@gmail.com>
2026-07-11 21:50:46 +0200

    first routing

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(f"CoffeeBlanket is alive ☕")
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return (f"CoffeeBlanket is alive ☕")
+
+@app.route("/about")
+def about():
+    return (f"This is About page")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
